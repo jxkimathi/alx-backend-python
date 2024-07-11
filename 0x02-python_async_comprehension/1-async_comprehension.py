7c32d0f7410b3f7f72be2b4022cd822c833a0d0b
jxkimathi <judescriva8@gmail.com>
2024-07-11 12:41:00 +0300

    correction on the import statement

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,11 @@
 """Writing a coroutine that returns 10 random numbers"""
 import asyncio
 from typing import List
-from random import uniform
+from importlib import import_module as using
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """Collects 10 random numbers and runs a coroutine"""
-    return [num async for num in async_generator]
+    return [num async for num in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,20 +2,19 @@
 """Writes a coroutine that will run a function 4 times"""
 import asyncio
 import time
-from typing import List
-from random import uniform
+from importlib import import_module as using
 
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async_comprehension = using('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """Measures the runtime of the coroutines"""
-    start_time = time.perf_counter()
+    start_time = time.time()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    end_time = time.perf_counter()
-    return end_time - start_time
+    return time.time() - start_time
