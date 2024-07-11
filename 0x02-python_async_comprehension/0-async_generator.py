5493bb44861eac8384aa522a7f8fdd47de81bb0a
jxkimathi <judescriva8@gmail.com>
2024-07-11 13:06:34 +0300

    correction on the random function in the loop

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> Generator[float, None, None]:
     """A coroutine generator"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform() * 10
+        yield random.random() * 10
